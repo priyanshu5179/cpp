bcf04b502392585a0578ee6418819cf57190cf04
priyanshu5179 <priyanshuyadav876585@gmail.com>
2025-09-18 16:26:16 +0530

    multiplication of two num

--- 2main.cpp
+++ 2main.cpp
@@ -1,7 +1,11 @@
-hello world 
+#include <iostream>
+using namespace std;
 
-
-dfzdfldjiofjdsifjdjfjioejtilesjfiesufiuesipof
-dflkdspofpoesiufpoesufiesufioesuesuufespufespo
-
-sfkodpofjdpofpfipodufeiufpedfuipeu
+int main() {
+    int a = 5, b = 6;
+    //int sum = a + b;
+    int multiply = (a*b);
+    //cout << "The sum of two values is: " << sum << endl;
+    cout << "multiplication of two number:" << multiply << endl;
+    return 0;
+}
