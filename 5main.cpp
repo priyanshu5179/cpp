587eaa0af7bd4c7690cb20ee678d829f05988e09
priyanshu5179 <priyanshuyadav876585@gmail.com>
2025-09-19 23:05:45 +0530

    use of while loop

---
+++ 5main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main (){
+    int n = 45;
+    int count = 1;
+    while(count<=n){
+        cout<< count <<endl;
+        count=count+1;
+
+    }
+    return 0;
+}
