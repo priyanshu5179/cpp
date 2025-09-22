b01eb9dde7da7c2d17c54ba75cb5d8e4e834cfd7
priyanshu5179 <priyanshuyadav876585@gmail.com>
2025-09-22 22:39:13 +0530

    finding prime num

--- 6main.cpp
+++ 6main.cpp
@@ -2,13 +2,12 @@
 using namespace std;
 int main (){
     int n=100;
-    int oddsum=0;
-    for(int i=1; i<=n; i++){
-        if(i%2!=0){
-           oddsum+=i;
-    }
-}
-    cout << oddsum << endl;
-    
+    bool isprime=true;
+    for(int i=2; i<=n-1; i++){
+        if(n%i==0){
+           isprime=false;
+           break;
+}   
     return 0;
+}
 }
