36bb38b8adf856b703268d841f8a5cea1cb968c1
priyanshu5179 <priyanshuyadav876585@gmail.com>
2025-10-04 21:44:52 +0530

    finding odd even

---
+++ ifelse/ifelse1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main (){
+    int n;
+    cout<< "enter a number:"<<endl;
+    cin>>n;
+    if (n%2==0){
+        cout<< "even";
+    }
+    else{
+        cout<<"odd";
+        
+}
+return 0;
+}
