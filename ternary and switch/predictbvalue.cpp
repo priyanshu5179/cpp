fe5b427a4108549d012ecf28b1fe392c6c8ab689
priyanshu5179 <priyanshuyadav876585@gmail.com>
2025-10-07 22:02:27 +0530

    finding value

---
+++ ternary and switch/predictbvalue.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main (){
+    char    x;
+    int a=10;
+    x=(a>0) ? 'a':'S';
+    cout<<"value of x: "<< x << endl;
+    cout<<"value of a:"<<a;
+    return 0;
+}
