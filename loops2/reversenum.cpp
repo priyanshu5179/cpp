367937c7fba58bdc10187b02bafb0c78fb6ac307
priyanshu5179 <priyanshuyadav876585@gmail.com>
2025-10-09 22:41:53 +0530

    finding reverse num sum

---
+++ loops2/reversenum.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main (){
+    int n;
+    cout << "enter a number ";
+    cin>>n;
+    int lastdigit=0;
+    int reverse=0;
+    while (n>0){
+        reverse*=10;
+        lastdigit=n%10;
+        reverse+=lastdigit;
+        n/=10;
+    }
+    cout <<reverse;
+}
