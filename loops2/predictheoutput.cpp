a83e13775f3fdf6a40671d23d27ca3b85d224458
priyanshu5179 <priyanshuyadav876585@gmail.com>
2025-10-09 22:43:25 +0530

    pridict the output

---
+++ loops2/predictheoutput.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main() {
+   // int x=4,y=0,z;
+    //while(x>=0){
+       // x--;
+        //y++;
+       // if (x==y) 
+         //   continue;
+        //else 
+          //  cout<<x<<" "<<y<<endl;
+          //if (x==y) break;
+         // else cout<<x<<" "<<y<<endl;
+         //    x--;
+           //  y++;
+          int t=10;
+          while (t/=2){ 
+            cout<<"hello"<<endl;
+          }
+          } 
