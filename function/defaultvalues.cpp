b4f9cb3b093a68317d6fa8b1228ca60d4d183f06
priyanshu5179 <priyanshuyadav876585@gmail.com>
2025-10-12 19:16:45 +0530

    checking defautlt values

--- function/defaultvalues.cpp
+++ function/defaultvalues.cpp
@@ -1,14 +1,10 @@
 #include <iostream>
 using namespace std;
 
-void fun(int x, int y) {
-    cout << x;
+void fun(int x=4, int y=5) {//when any default values not gives then show error 
+    cout << x<<" "<<y;
     }
-    int main (){
-        int  x=4;
-        int y=5;
-        cout<<x<<y;
-    
-    
-    
-    }
+int main (){
+    fun(6,7);
+}
+  
